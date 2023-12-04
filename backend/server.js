e5041c78c1bcba77fb4482be2e4dd9dd1eb15a45
Islam Saeed <islamsaeed111998@gmail.com>
2023-12-04 04:06:23 -0800

    fix for phone browsers

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
-const http = require("http");
-const { Server } = require("socket.io");
-const express = require("express");
-const app=express();
-const cors=require("cors");
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const express = require("express");
+// const app=express();
+// const cors=require("cors");
 
-app.use(cors());
+// app.use(cors());
 
-const httpServer = http.createServer(app);
+// const httpServer = http.createServer(app);
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:3001",
-    methods: ["GET", "POST"]
-  }
-});
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: "http://localhost:3001",
+//     methods: ["GET", "POST"]
+//   }
+// });
+const io = require('socket.io')(3000)
 
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
@@ -23,6 +24,6 @@ io.on('connection', (socket) => {
   });
 });
 
-httpServer.listen(3000, ()=>{
-  console.log("Server is running on port 3000");
-})
+// httpServer.listen(3000, ()=>{
+//   console.log("Server is running on port 3000");
+// })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { IoSend } from "react-icons/io5";
 
 function App() {
 
-  const socketUrl = 'http://localhost:3000';
+  const socketUrl = process.env.REACT_APP_SERVER_URL;
   let socket = useRef(null);
   const [inputText, setInputText] = useState('');
   const [ messages, setMessages] = useState([]);
@@ -38,8 +38,8 @@ function App() {
   };
 
   return (
-    <div className="App bg-[#222] text-white h-[100vh] w-[100%] flex justify-center items-center flex-col-reverse">
-      <div id="chat-container" className='w-[80vw] h-[90vh] bg-[#111] rounded flex flex-col-reverse p-3'>
+    <div className="App bg-[#222] text-white h-[100vh] w-[100%] flex sm:justify-center sm:items-center flex-col">
+      <div id="chat-container" className='sm:w-[80vw] w-[100vw] sm:h-[90vh] h-[100%] bg-[#111] rounded flex flex-col-reverse p-3'>
         <form className='flex justify-around' onSubmit={e=>handleSubmit(e)}>
           <input type="text" id="message-input" className='bg-[#333] rounded-full w-[90%] py-2 px-4 focus:outline-none' placeholder='type a message' autoComplete='off' value={inputText} onChange={(e)=>setInputText(e.target.value)}/>
           <button type="submit" id="send-button" className='rounded-full bg-[#C40234] w-12 h-12 flex justify-center items-center text-xl'><IoSend /></button>
