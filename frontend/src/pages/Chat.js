db89dd09e72467a058974728ca66da33204baea6
Islam Saeed <islamsaeed111998@gmail.com>
2024-01-14 20:07:24 -0800

    adding logout

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -1,10 +1,21 @@
 import ChatBox from '../components/ChatBox';
 import Sidebar from '../components/Sidebar';
+import { IoLogOutOutline } from "react-icons/io5";
+import { useCookies } from 'react-cookie';
+import { useContext } from 'react';
+import { userContext } from '../context/UserContext';
 
 const Chat = () => {
-    
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
+    const [user, setUser] = useContext(userContext)
     return (
       <div className='grid grid-cols-12 gap-4'>
+        <div className='absolute right-5 top-10 flex justify-center items-center text-white text-5xl cursor-pointer'>
+          <IoLogOutOutline onClick={()=> {
+            setUser({})
+            removeCookie('user');
+          }} />
+        </div>
         <Sidebar />
         <ChatBox />
       </div>
