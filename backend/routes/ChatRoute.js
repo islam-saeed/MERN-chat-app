b9fc0796aa054d662c84a983f284d1a3cfe1917f
Islam Saeed <islamsaeed111998@gmail.com>
2024-01-25 22:05:07 -0800

    removing unnecessary routes

--- backend/routes/ChatRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { createChat, findChat, userChats } = require('../controllers/ChatController.js');
-const router = express.Router()
-
-router.post('/', createChat);
-router.get('/:userId', userChats);
-router.get('/find/:firstId/:secondId', findChat);
-
-module.exports = router
